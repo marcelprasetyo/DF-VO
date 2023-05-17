4be1ced23b2c2598d985efe1b8dfdefc1ebaf97f
marcelprasetyo <1003632@mymail.sutd.edu.sg>
2023-05-17 13:39:33 +0800

    Added result folder with correspondence files for each sequence, and evaluation scripts

---
+++ result_copy/eval-cmd.sh
@@ -0,0 +1,8 @@
+python eval_dfvo.py \
+--base_path 4seasons/12-22 \
+--est_path 4seasons/12-22/run1/12-22-undistort.txt \
+--gt_path /home/marcelbp/Desktop/Oxford-dataset/Rain_Datasets/12-22-undistort/undistorted_images/stereo_gt_nocrop_4seasons_12-22.out \
+--gt_ts_path /home/marcelbp/Desktop/Oxford-dataset/Rain_Datasets/12-22-undistort/undistorted_images/gt_ts_nocrop_4seasons_12-22.out \
+--save_plot_path 4seasons/12-22/run1/12-22_run1_plot.png \
+--timestamp_divide_by_digits 9 \
+--save_file 4seasons/12-22/run1/12-22_eval_run1_result.txt
